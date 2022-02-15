130aec510e915cc3c1c26f925de21f38cb7dc911
Robson Leal ArchPc <devleal@hotmail.com>
2022-02-15 16:31:12 -0300

    Adcionando novos modelos ao app e cadastrando os modelos no admin

--- medicSearchAdmin/settings/settings.py
+++ medicSearchAdmin/settings/settings.py
@@ -87,6 +87,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+# Media files (Imagens salvas no Banco de Dados)
+MEDIA_URL = '/media/' 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
