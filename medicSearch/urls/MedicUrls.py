6faa08e13982db5af784a47f3c7c3a2dd775cfb1
Robson Leal ArchPc <devleal@hotmail.com>
2022-02-17 16:42:05 -0300

    Adicionando funçao de adcionar medico aos favoritos

--- medicSearch/urls/MedicUrls.py
+++ medicSearch/urls/MedicUrls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from medicSearch.views.MedicView import list_medic_view
+from medicSearch.views.MedicView import list_medic_view, add_favorite_view
 
 urlpatterns = [
     path('', list_medic_view, name='medics'),
+    path('favorite', add_favorite_view, name='medic-favorite')
 ]
