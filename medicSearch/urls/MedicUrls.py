41af9f369d424edc92128771fd7acc9a7e586f4d
Robson Leal ArchPc <devleal@hotmail.com>
2022-02-18 13:24:01 -0300

    Criando template de perfil, para os usuarios do sistema; Adicionando funcionalidade de remover ou adicionar favoritos

--- medicSearch/urls/MedicUrls.py
+++ medicSearch/urls/MedicUrls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from medicSearch.views.MedicView import list_medic_view, add_favorite_view
+from medicSearch.views.MedicView import list_medic_view, add_favorite_view, remove_favorite_view
 
 urlpatterns = [
     path('', list_medic_view, name='medics'),
-    path('favorite', add_favorite_view, name='medic-favorite')
+    path('favorite', add_favorite_view, name='medic-favorite'),
+    path('favorite/remove', remove_favorite_view, name='medic-favorite-remove'),
 ]
--- medicSearch/views/ProfileView.py
+++ medicSearch/views/ProfileView.py
@@ -1,8 +1,43 @@
-from django.http import HttpResponse
-
-def list_profile_view(request, id=None):
+from django.shortcuts import render, redirect, get_object_or_404
+from medicSearch.models import Profile
+from medicSearch.forms.UserProfileForm import UserProfileForm
+from django.core.paginator import Paginator
+  
+def list_profile_view(request, id=None): 
+    profile = None 
     if id is None and request.user.is_authenticated: 
-        id = request.user.id 
+        profile = Profile.objects.filter(user=request.user).first() 
+    elif id is not None: 
+        profile = Profile.objects.filter(user__id=id).first() 
     elif not request.user.is_authenticated: 
-        id = 0
-    return HttpResponse('<h1>Usuário de id %s!<h1/>' % id)
+        return redirect(to='/') 
+  
+    favorites = profile.show_favorites() 
+    if len(favorites) > 0: 
+        paginator = Paginator(favorites, 8) 
+        page = request.GET.get('page') 
+        favorites = paginator.get_page(page)
+
+    ratings = profile.show_ratings() 
+    if len(ratings) > 0: 
+        paginator = Paginator(ratings, 8) 
+        page = request.GET.get('page') 
+        ratings = paginator.get_page(page) 
+  
+    context = {        
+        'profile':profile, 
+        'favorites':favorites,
+        'ratings':ratings
+    } 
+    
+    return render(request, template_name='profile/profile.html', context=context, status=200)
+
+def edit_profile(request): 
+    profile = get_object_or_404(Profile, user=request.user) 
+    profileForm = UserProfileForm(instance=profile) 
+  
+    context = { 
+        'profileForm': profileForm 
+    } 
+  
+    return render(request, template_name='user/profile.html', context=context, status=200)
