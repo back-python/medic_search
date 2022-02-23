502763ab665fda95af0b2a41ee7a2ae17e40bcab
Robson Leal <devleal@hotmail.com>
2022-02-22 22:25:54 -0300

    Deploy da aplicacao

--- medicSearchAdmin/settings/production.py
+++ medicSearchAdmin/settings/production.py
@@ -5,7 +5,7 @@ DEBUG = True
 # Crie a secret key para seu ambiente de produção 
 SECRET_KEY = 'ixb62ha#ts=ab4t2u%p1_62-!5w2j==j6d^3-j$!z(@*m+-h' 
 # Adicionar o endereço para ambiente de produção, quando houver
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://medic-search.herokuapp.com/']
 
 DATABASES = {    
     'default':{ 
