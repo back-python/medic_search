464e700bc6ca36315e46567f7c35590cf771012f
Robson Leal <devleal@hotmail.com>
2022-02-22 23:55:40 -0300

    Configurando banco de dados deploy

--- medicSearchAdmin/settings/production.py
+++ medicSearchAdmin/settings/production.py
@@ -7,23 +7,13 @@ SECRET_KEY = 'ixb62ha#ts=ab4t2u%p1_62-!5w2j==j6d^3-j$!z(@*m+-h'
 # Adicionar o endereço para ambiente de produção, quando houver
 ALLOWED_HOSTS = ['medic-search.herokuapp.com']
 
-DATABASES = {    
+DATABASES = { 
     'default':{ 
-        'ENGINE': 'django.db.backends.sqlite3', 
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'), 
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',        
+        'NAME': 'medics', 
+        'USER': 'root',
+        'PASSWORD': '123',        
+        'HOST': 'localhost', 
+        'PORT': '5432', 
+    } 
 }
-
-# Para conexão com PostgreSQL
-# Não esquecer de instalar o psycopg2
-
-# DATABASES = { 
-#     'default':{ 
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',        
-#         'NAME': 'Nome do seu banco de dados', 
-#         'USER': 'Nome do usuário',
-#         'PASSWORD': 'Senha',        
-#         'HOST': 'Nome do host de conexão ao banco', 
-#         'PORT': 'Número da porta de comunicação', 
-#     } 
-# }
